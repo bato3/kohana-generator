77dc08b3cb61c68ca4ad8345c742a7901122b7bd
burningface <burningface11@gmail.com>
2012-08-03 10:22:15 +0200

    2012-08-03-1

--- init.php
+++ init.php
@@ -1,11 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-//if(Kohana::$environment !== Kohana::PRODUCTION)
-//{	
-    // Static file serving (CSS, JS, images)
+
     Route::set('gmedia', 'gmedia(/<file>)', array('file' => '.+'))
 	->defaults(array(
             'controller' => 'gmedia',
             'file'       => NULL,
     ));
-
-//}
+    Route::set('generator', '(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'controller' => 'welcome',
+		'action'     => 'index',
+	));
+?>
